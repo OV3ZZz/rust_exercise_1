840ef1aebf66df18dab94a29f8047a91f40b2322
OV3ZZz <opeitsalo@gmail.com>
2025-06-03 20:42:57 +0300

    "Hey, Rust!"

---
+++ exercise_1/target/debug/deps/exercise_1-199bc963f1aa4470.d
@@ -0,0 +1,5 @@
+C:\Users\Omistaja\OneDrive\Työpöytä\rust_exercise_1\exercise_1\target\debug\deps\libexercise_1-199bc963f1aa4470.rmeta: src\main.rs
+
+C:\Users\Omistaja\OneDrive\Työpöytä\rust_exercise_1\exercise_1\target\debug\deps\exercise_1-199bc963f1aa4470.d: src\main.rs
+
+src\main.rs:
---
+++ exercise_1/target/debug/deps/exercise_1-ee1e338446cc6264.d
@@ -0,0 +1,5 @@
+C:\Users\Omistaja\OneDrive\Työpöytä\rust_exercise_1\exercise_1\target\debug\deps\libexercise_1-ee1e338446cc6264.rmeta: src\main.rs
+
+C:\Users\Omistaja\OneDrive\Työpöytä\rust_exercise_1\exercise_1\target\debug\deps\exercise_1-ee1e338446cc6264.d: src\main.rs
+
+src\main.rs:
